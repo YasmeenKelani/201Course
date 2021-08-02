6fb2875fff6398a018a2d01cbca0f304a42276c4
YasmeenKelani <yasminekailani98@gmail.com>
2021-08-02 16:43:28 +0300

    About me

---
+++ js/app.js
@@ -0,0 +1,74 @@
+'use strict'
+
+
+let name = prompt("What is your name?").toLowerCase();
+alert("Welcome to my website" + name)
+
+
+let painting = prompt("Do you think I can draw? yes,no")
+if(painting == "yes"){
+    //console.log("correct");
+    alert("correct") 
+}
+else if (painting == "no"){
+    //console.log("incorrect");
+    alert("incorrect")
+}
+
+
+let styling = prompt("Do you think I like styling using CSS?yes,no")
+if(styling == "yes"){
+    //console.log("correct");
+    alert("correct") 
+}
+else if (styling== "no"){
+    //console.log("incorrect");
+    alert("incorrect")
+}
+
+
+
+let colors = prompt("Do you think that My favorite color is blue? yes,no")
+if(colors == "yes"){
+    //console.log("correct");
+    alert("correct") 
+}
+else if (colors == "no"){
+    //console.log("incorrect");
+    alert("incorrect")
+}
+
+
+let tarvel = prompt("Do I like traviling? yes,no")
+if(travel == "yes"){
+    //console.log("correct");
+    alert("correct") 
+}
+else if (travel == "no"){
+    //console.log("incorrect");
+    alert("incorrect")
+}
+
+
+let personality = prompt("Do you think that I'm a logical person? yes,no")
+if(personality == "yes"){
+    //console.log("correct");
+    alert("correct") 
+}
+else if (personality == "no"){
+    //console.log("incorrect");
+    alert("incorrect")
+}
+
+alert("Thank you for using my website" + name)
+
+
+
+
+
+
+
+
+
+
+
