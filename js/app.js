f7bc1904cc403cd20c1836442831b945a0b90c25
YasmeenKelani <yasminekailani98@gmail.com>
2021-08-03 17:33:20 +0300

    About me

--- js/app.js
+++ js/app.js
@@ -1,75 +1,170 @@
 'use strict';
 
 
-let name = prompt("What is your name?").toLowerCase();
+let name = prompt("What is your name?");
 
-alert("Welcome to my website" + " " +  name)
+alert("Welcome to my website" + " " + name)
 
+alert("This is my gussing game")
+let score = 0;
 
 let painting = prompt("Do you think I can draw? yes,no").toLowerCase();
-if(painting == "yes"){
-    //console.log("correct");
-    alert("correct") 
-}
-else if (painting == "no"){
-    //console.log("incorrect");
-    alert("incorrect")
+
+switch (painting) {
+    case 'yes':
+    case 'y':
+        console.log('correct');
+        alert("correct");
+        score+=1;
+        break;
+
+    case 'no':
+    case 'n':
+        alert("incorrect answer");
+        break;
+
+    default:
+        alert("please answer yes or no ");
 }
 
 
 let styling = prompt("Do you think I like styling using CSS?yes,no").toLowerCase();
-if(styling == "yes"){
-    //console.log("correct");
-    alert("correct") 
+
+switch (styling) {
+    case 'yes':
+    case 'y':
+        console.log('correct');
+        alert("correct");
+        score+=1;
+        break;
+
+    case 'no':
+    case 'n':
+        alert("incorrect answer");
+        break;
+
+    default:
+        alert("None");
 }
-else if (styling== "no"){
-    //console.log("incorrect");
-    alert("incorrect")
-}
+
 
 
 
 let colors = prompt("Do you think that My favorite color is blue? yes,no").toLowerCase();
-if(colors == "yes"){
-    //console.log("correct");
-    alert("correct") 
-}
-else if (colors == "no"){
-    //console.log("incorrect");
-    alert("incorrect")
+
+
+switch (colors) {
+    case 'yes':
+    case 'y':
+        console.log('correct');
+        alert("correct");
+        score+=1;
+        break;
+
+    case 'no':
+    case 'n':
+
+        alert("incorrect answer");
+        break;
+
+    default:
+        alert("None");
 }
 
 
 let tarvel = prompt("Do I like traviling? yes,no").toLowerCase();
-if(travel == "yes"){
-    //console.log("correct");
-    alert("correct") 
+
+switch (tarvel) {
+    case 'yes':
+    case 'y':
+        console.log('correct');
+        alert("correct");
+        score+=1;
+        break;
+
+    case 'no':
+    case 'n':
+
+        alert("incorrect answer");
+        break;
+
+    default:
+        alert("None");
 }
-else if (travel == "no"){
-    //console.log("incorrect");
-    alert("incorrect")
-}
+
 
 
 let personality = prompt("Do you think that I'm a logical person? yes,no").toLowerCase();
-if(personality == "yes"){
-    //console.log("correct");
-    alert("correct") 
+
+switch (personality) {
+    case 'yes':
+    case 'y':
+        console.log('correct');
+        alert("correct");
+        score+=1;
+        break;
+
+    case 'no':
+    case 'n':
+
+        alert("incorrect answer");
+        break;
+
+    default:
+        alert("None");
 }
-else if (personality == "no"){
-    //console.log("incorrect");
-    alert("incorrect")
+
+let num
+for (let i = 0; i < 4; i++) {
+    num = Number(prompt("guess my age?"))
+
+    if (num === 23) {
+        alert("That's correct");
+        console.log("correct answer " + num);
+        score+=1;
+        break;
+    }
+    else if (num > 23) {
+        alert("too high")
+    }
+    else if (num < 23) {
+        alert("too low")
+    }
+
 }
 
-alert("Thank you for visiting my Website" + " " + name)
+if (num != 23)
+    alert("My age is 23")
 
 
+ let  places 
+let favplaces = ['France', 'Tokyo', 'Colombia', 'Italy', 'Singapore', 'Maldives', 'New Zeland', 'Bali', 'Cyprus', 'United Kingdom'];
 
+for (let x = 0; x < 6; x++) {
+      places = (prompt("What is my fav places I want to visit?"))
+//    console.log(places + " user answer");
+let j;
+    for ( j = 0; j < favplaces.length; j++) {
+        // console.log(favplaces[j] + "correct answer");
 
+        if (places == favplaces[j]) {
+            alert("That's corresct");
+            console.log("correct answer" + places);
+            score+=1;
+          
+            break;
+        } 
 
+    }
+    if(places == favplaces[j]){
+        break;
+    }
+    else{
+        alert("incorrect answer");
+        
+    }
+}
 
-
-
-
-
-
+alert("You finished your attempet and this is the correct answer: France, Tokyo, Colombia, Italy, Singapore, Maldives, New Zeland, Bali, Cyprus, United Kingdom");
+alert("you score is " + score +" out of 7");
+alert("Thank you for visiting my Website " + name);
